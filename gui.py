a4ae61f16e40eb6d3d7b8079d1b18fd8976f0c49
SunnyInspired <sundayelesi@gmail.com>
2023-03-04 23:12:05 +0100

    Implemented the Edit Todo button

--- gui.py
+++ gui.py
@@ -4,12 +4,15 @@ import PySimpleGUI as App
 label = App.Text("Enter A Todo Item")
 input_box = App.InputText(tooltip='Enter Todo', key="todo")
 add_button = App.Button("Add Todo")
+list_box = App.Listbox(values=fn.get_todos(), key="todos", enable_events=True,
+                       size=(45, 10))
+edit_button = App.Button("Edit Todo")
 
 layout = [
-    [label], [input_box, add_button]
+    [label], [input_box, add_button], [list_box, edit_button]
 ]
 
-window = App.Window('Todo App', layout=layout, font=("cambria", 14))
+window = App.Window('Todo App', layout=layout, font=("Ubuntu", 14))
 while True:
     event, value = window.read()
     print(event)
@@ -21,6 +24,21 @@ while True:
             todos.append(new_todo)
             # write the new todos to file
             fn.write_todos(todos)
+            window["todos"].update(values=todos)
+            window["todo"].update(value="")
+        case "Edit Todo":
+            todo_to_edit = value["todos"][0]
+
+            new_todo = value['todo']+"\n"
+
+            todos = fn.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+
+            fn.write_todos(todos)
+            window["todos"].update(values=todos)
+        case "todos":
+            window["todo"].update(value=value["todos"][0])
         case App.WIN_CLOSED:
             break
 
