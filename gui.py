fbf5a6c50a36c06989556ab47b888a2184c84c8f
SunnyInspired <sundayelesi@gmail.com>
2023-03-06 22:03:05 +0100

    Implemented Button Image and improved the app

--- gui.py
+++ gui.py
@@ -5,7 +5,8 @@ import time
 clock = App.Text('', key="clock", font=("Helvetica", 14))
 label = App.Text("Enter A Todo Item")
 input_box = App.InputText(tooltip='Enter Todo', key="todo")
-add_button = App.Button("Add Todo")
+add_button = App.Button(key="Add Todo", image_source="resources/add.png",
+                        button_color="#64748c", mouseover_colors="Lavender")
 list_box = App.Listbox(values=fn.get_todos(), key="todos", enable_events=True,
                        size=(45, 10))
 edit_button = App.Button("Edit Todo")
@@ -23,12 +24,15 @@ while True:
     match event:
         case "Add Todo":
             todos = fn.get_todos()
-            new_todo = value["todo"] + "\n"
-            todos.append(new_todo)
-            # write the new todos to file
-            fn.write_todos(todos)
-            window["todos"].update(values=todos)
-            window["todo"].update(value="")
+            if value["todo"] != "":
+                new_todo = value["todo"] + "\n"
+                todos.append(new_todo)
+                # write the new todos to file
+                fn.write_todos(todos)
+                window["todos"].update(values=todos)
+                window["todo"].update(value="")
+            else:
+                App.popup("Please enter a Todo Item!", font=("Ubuntu", 12))
         case "Edit Todo":
             try:
                 todo_to_edit = value["todos"][0]
